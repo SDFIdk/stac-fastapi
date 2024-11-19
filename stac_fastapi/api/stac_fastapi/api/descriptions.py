033b1b1cd0dc7bc7aa6f0614740e261c67090859
seskildsen <sines@sdfi.dk>
2024-11-19 14:32:52 +0100

    docs: have descriptions on one line

--- stac_fastapi/api/stac_fastapi/api/descriptions.py
+++ stac_fastapi/api/stac_fastapi/api/descriptions.py
@@ -11,38 +11,27 @@ PAGING_TOKEN = "Token used for paging functionality. Only use tokens as returned
 
 IDS = "Array of Item ids to return. All other filter parameters that further restrict the number of search results are ignored"
 
-BBOX = """Only return items intersecting this bounding box. Mutually exclusive with `intersects`. The default sort is the shortest distance between specifed geometry centroid and the footprint centroid of skraafotos. 
-
+BBOX = """Only return items intersecting this bounding box. Mutually exclusive with `intersects`. The default sort is the shortest distance between specifed geometry centroid and the footprint centroid of skraafotos. See also `bbox-crs`.
 Example: `10.312385559082033,55.33392904334293,10.36388397216797,55.353452174893285`
-
-See also `bbox-crs`.
 """
 
 BBOX_CRS = """The coordinate reference system (CRS) used by `bbox` param. The specified CRS must be supported by the server."""
 
 INTERSECTS = """Only return items intersecting this GeoJSON Geometry. Mutually exclusive with `bbox`. The default sort is the shortest distance between specifed geometry centroid and the footprint centroid of skraafotos. """
 
-DATETIME = """Only return items that have a temporal property that intersects this value.
-
-Either a date-time or an interval, open or closed. Date and time expressions adhere to RFC 3339. Open intervals are expressed using double-dots.
-
+DATETIME = """Only return items that have a temporal property that intersects this value. Either a date-time or an interval, open or closed. Date and time expressions adhere to RFC 3339. Open intervals are expressed using double-dots.
 Examples:
 - A date-time: `2018-02-12T23:20:50Z`
 - A closed interval: `2018-02-12T00:00:00Z/2018-03-18T12:31:12Z`
 - Open intervals: `2018-02-12T00:00:00Z/..` or `../2018-03-18T12:31:12Z`
 """
 
-FILTER = """A CQL filter expression for filtering items.
-
-Supports `CQL-JSON` as defined in https://portal.ogc.org/files/96288
-
+FILTER = """A CQL filter expression for filtering items. Supports `CQL-JSON` as defined in https://portal.ogc.org/files/96288. If filter input has a geometry, the default sort is the shortest distance between specifed geometry centroid and the footprint centroid of skraafotos. 
+Remember to URL encode the CQL-JSON if using GET `/search` or `/collections/{collectionid}/items`.
 Example:
 ```
 { "gt": [ { "property": "gsd" }, "0.1" ] }
 ```
-
-If filter input has a geometry, the default sort is the shortest distance between specifed geometry centroid and the footprint centroid of skraafotos. 
-Remember to URL encode the CQL-JSON if using GET `/search` or `/collections/{collectionid}/items`.
 """
 
 FILTER_LANG = "The CQL filter encoding that the 'filter' value uses. This server only supports the encoding `cql-json`"
@@ -50,7 +39,6 @@ FILTER_LANG = "The CQL filter encoding that the 'filter' value uses. This server
 FILTER_CRS = """The coordinate reference system (CRS) used by spatial literals in the 'filter' value. The specified CRS must be supported by the server."""
 
 SORTBY="""An array of property names, prefixed by either "+" for ascending or "-" for descending. If no prefix is provided, "+" is assumed. If GET request remember to urlencode the prefix.
-
 Example: `-gsd,-datetime`
 """
 
